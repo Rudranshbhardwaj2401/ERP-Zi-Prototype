e3bfa9ce618ddbe2bbbecd1abefd3a0b3d1bcdfa
Rudranshbhardwaj2401 <138233198+Rudranshbhardwaj2401@users.noreply.github.com>
2025-09-09 10:01:04 +0530

    Added Loading Screen, Enhanced the overall view

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("loader").style.animation = "fadeOut 1s forwards";
+        setTimeout(() => {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("main").style.display = "block";
+        }, 1000);
+    }, 3000); // matches the bar animation
+});
+
 function setRole(btn) {
     document.querySelectorAll("button").forEach(b => b.classList.remove("active"))
     btn.classList.add("active")
